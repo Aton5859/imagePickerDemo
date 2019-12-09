5774c1c10be1742b4c89c965fa0f26ce12057555
Aton5859 <390072015@qq.com>
2019-12-09 16:37:55 +0800

    具体实现
    1、ionic cordova plugin add cordova-plugin-telerik-imagepicker --variable PHOTO_LIBRARY_USAGE_DESCRIPTION="your usage message"
    2、npm install --save @ionic-native/image-picker@4
    切记在ionic3中要使用@ionic-native/image-picker版本4，高版本亲测有兼容问题！

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { ImagePicker, ImagePickerOptions } from '@ionic-native/image-picker';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,30 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(
+    public navCtrl: NavController,
+    private imagePicker: ImagePicker
+  ) {
 
+  }
+
+  pick() {
+    try {
+      let options: ImagePickerOptions = {
+        maximumImagesCount: 10,
+        width: 100,
+        height: 100,
+        quality: 10,
+        outputType: 1
+      };
+      this.imagePicker.getPictures(options).then((results) => {
+        for (var i = 0; i < results.length; i++) {
+          alert('Image URI: ' + results[i]);
+        }
+      }, (err) => { });
+    } catch (error) {
+      alert(error);
+    }
   }
 
 }
